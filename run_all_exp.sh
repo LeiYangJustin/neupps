8063ebdb49af833894755f75f8864a0edebe53cc
leiyangjustin <yanglei.dalian@gmail.com>
2026-06-04 21:17:00 +0800

    update

--- run_all_exp.sh
+++ run_all_exp.sh
@@ -1,4 +1,3 @@
-# python main.py --config configs/config.json --model_name kitten_20241010_2320 --duv
 python main.py --config configs/config.json --model_name bimba --duv
 python main.py --config configs/config.json --model_name SL_Skirt013_0 --duv  
-python main.py --config configs/config.json --model_name david --duv
+python main_pc.py --config configs/config_noparam.json --model_name pointcloud_skirt_529 --duv
